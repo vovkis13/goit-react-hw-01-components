8b2b870339231f20582806a97b403e62efda750b
Volodymyr Klochan <vovkis13@gmail.com>
2021-11-25 14:48:49 +0200

    add App component

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import user from "./data/user.json";
-import data from "./data/data.json";
-import friends from "./data/friends.json";
-import transactions from "./data/transactions.json";
-import Profile from "./Profile/Profile";
-import Statistics from "./Statistics/Statistics";
-import FriendList from "./Friendlist/FriendList";
-import TransactionHistory from "./Transactionhistory/TransactionHistory";
-import s from "./App.module.css";
+import React from 'react';
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
+import Profile from '../Profile/Profile';
+import Statistics from '../Statistics/Statistics';
+import FriendList from '../Friendlist/FriendList';
+import TransactionHistory from '../Transactionhistory/TransactionHistory';
+import s from './App.module.css';
 
 export default function App() {
   return (
