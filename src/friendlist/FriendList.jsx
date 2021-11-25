a02cc88c029c7aebf1dde496880cde63d9dedee2
Volodymyr Klochan <vovkis13@gmail.com>
2021-11-25 22:47:09 +0200

    Update FriendList.jsx

--- src/friendlist/FriendList.jsx
+++ src/friendlist/FriendList.jsx
@@ -1,20 +1,26 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
 import s from './FriendList.module.css';
 
-const FriendList = ({friends}) => (
+const FriendList = ({ friends }) => (
   <ul className={s.friendList}>
     {friends.map(friend => (
       <li className={s.item} key={friend.id}>
         <span
-          className={s.status + ' ' + (friend.isOnline ? s.online : s.offline)}
+          className={s.status + '  ' + (friend.isOnline ? s.online : s.offline)}
         ></span>
-        <img className={s.avatar} src={friend.avatar} alt={friend.name} width="48" />
+        <img
+          className={s.avatar}
+          src={friend.avatar}
+          alt={friend.name}
+          width="48"
+        />
         <p className={s.name}>{friend.name}</p>
       </li>
     ))}
   </ul>
 );
+
 FriendList.propTypes = {
   friends: PropTypes.array.isRequired,
 };
